ec46edcd8b5db0fe77555b6c1ac63e9bc2f20420
Amandeep Kaur <000822179@mohawkcollege.ca>
2022-08-10 21:55:59 -0400

    Created Tutor Account page

---
+++ Tutoring_Platform/ClientApp/src/components/TutorAccount.js
@@ -0,0 +1,13 @@
+﻿import React, { Component } from 'react';
+
+export class TutorAccount extends Component {
+    static displayName = TutorAccount.name;
+
+    render() {
+        return (
+            <div>
+                <h3>Tutor</h3>
+            </div>
+        );
+    }
+}
