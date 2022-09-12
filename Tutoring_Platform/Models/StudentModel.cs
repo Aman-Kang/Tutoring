e9693b2a0d7d7ac22d75d56dff4c16a43f7ccf77
Amandeep Kaur <000822179@mohawkcollege.ca>
2022-09-11 21:19:43 -0400

    Fetching Tutors from Database -> Created Model

--- Tutoring_Platform/Models/StudentModel.cs
+++ Tutoring_Platform/Models/StudentModel.cs
@@ -1,63 +1,48 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
 using System.Linq;
 namespace Tutoring_Platform.Models
 {
     public class StudentModel
     {
         tutoringContext db = new tutoringContext();
-        public void searchTutors(String course, List<bool> days)
+        public String[,] searchTutors(String course, List<bool> days)
+
         {
-            List<TutorCourse> tutorCourses = db.TutorCourses.Where(t => t.Course.Equals(course)).ToList();
-            List<int> tutorIds = new List<int>();
-            for(int i = 0; i < tutorCourses.Count; i++)
+            var tutorCourses = from t in db.TutorCourses
+                                          where t.Course.Contains(course)
+                                          select new { tId = t.TutorId,tCourse = t.Course };
+            var daysAvailable = from d in db.DaysAvailables
+                                             where d.Sunday == days[0] && d.Monday == days[1] && d.Tuesday == days[2] && d.Wednesday == days[3]
+                                             && d.Thursday == days[4] && d.Friday == days[5] && d.Saturday == days[6]
+                                             select new { d.UserId };
+            var tutorInfo = db.TutorInfos.Where(ti => tutorCourses.Any(tc => ti.Id == tc.tId) && daysAvailable.Any(da => ti.Id == da.UserId)).Select(ti => new { ti.Id,ti.Status,ti.Wage});
+            var studTutorInfo = db.StudTutorInfos.Where(sti => tutorInfo.Any(ti => sti.Id == ti.Id)).Select(sti => new { sti.Id , sti.School,sti.Program});
+            var user = db.Users.Where(u => studTutorInfo.Any(sti => u.Id == sti.Id));
+            String[,] returnValue = new String[user.Count(),6];
+            int i = 0;
+            foreach(var u in user)
             {
-                tutorIds.Add(tutorCourses[i].TutorId);
+                returnValue[i, 0] = u.FirstName.ToString();
+                returnValue[i, 1] = u.LastName.ToString();
+                i += 1;
+            }
+            i = 0;
+            foreach (var s in studTutorInfo)
+            {
+                returnValue[i, 2] = s.School.ToString();
+                returnValue[i, 3] = s.Program.ToString();
+                i += 1;
+            }
+            i = 0;
+            foreach (var t in tutorInfo)
+            {
+                returnValue[i, 4] = t.Status.ToString();
+                returnValue[i, 5] = t.Wage.ToString();
+                i += 1;
             }
 
-            List<int> tutorIds2 = new List<int>();
-            List<DaysAvailable> daysAvailable = db.DaysAvailables.Where(d => d.Sunday == days[0] && d.Monday == days[1] && d.Tuesday == days[2] && d.Wednesday == days[3] 
-            && d.Thursday == days[4] && d.Friday == days[5] && d.Saturday == days[6]).ToList();
-            for (int i = 0; i < daysAvailable.Count; i++)
-            {
-                tutorIds2.Add(daysAvailable[i].UserId);
-            }
-            List<int> tutorIds3 = new List<int>();
-            for (int i = 0; i < tutorIds.Count; i++)
-            {
-                for (int j = 0; i < tutorIds2.Count; i++)
-                {
-                   if(tutorIds[i] == tutorIds2[j])
-                    {
-                        tutorIds3.Add(tutorIds2[j]);
-                    }
-                }
-
-            }
-
-            List<TutorInfo> tutorInfo = new List<TutorInfo>();
-            for (int i = 0; i < tutorIds3.Count; i++)
-            {
-                tutorInfo.Add(db.TutorInfos.Where(t => t.Id == tutorIds3[i]).FirstOrDefault());
-            }
-            List<StudTutorInfo> studTutorInfo = new List<StudTutorInfo>();
-            for (int i = 0; i < tutorInfo.Count; i++)
-            {
-                for (int j = 0; i < db.StudTutorInfos.Count(); i++)
-                {
-                    try
-                    {
-                        if (tutorInfo[i].Id == db.StudTutorInfos.Find(j).UserId)
-                        {
-                            studTutorInfo.Add(db.StudTutorInfos.Find(j));
-                        }
-                    }
-                    catch
-                    {
-
-                    }
-                    
-                }
-            }
+            return returnValue;
         }
 
     }
